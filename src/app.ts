cba907df0fe1db766e86f8ff1021e4dc77b1e36c
Adam3192 <jeremyadixon@hotmail.com>
2022-08-25 16:53:32 -0500

    lesson 11 tokens

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import morgan from 'morgan';
 import { db } from './models';
 import messageRoutes from './routes/messageRoutes';
+import userRoutes from './routes/userRoutes';
 
 const app = express();
 
@@ -12,13 +13,15 @@ app.use(express.urlencoded({extended: true}));
 
 // routes
 app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.status(404).end();
 });
 
 // Syncing our database
-db.sync().then(() => {
+
+db.sync({ alter: true }).then(() => {
     console.info("connected to the database!")
 });
 
